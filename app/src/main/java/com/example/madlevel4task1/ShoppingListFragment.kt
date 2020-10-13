54460225f5d80e3bd3553544a4e87d3d7d14b63c
Joep Boekhold <joepboekhold@gmail.com>
2020-10-13 15:31:12 +0200

    3.2

--- app/src/main/java/com/example/madlevel4task1/ShoppingListFragment.kt
+++ app/src/main/java/com/example/madlevel4task1/ShoppingListFragment.kt
@@ -5,12 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_shopping_list.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 
 private lateinit var productRepository: ProductRepository
+private val mainScope = CoroutineScope(Dispatchers.Main)
+
+private val products = arrayListOf<Product>()
+private val shoppingListAdapter = ShoppingListAdapter(products)
 
 class ShoppingListFragment : Fragment() {
 
@@ -30,6 +44,41 @@ class ShoppingListFragment : Fragment() {
     }
 
     private fun initRv() {
-        TODO("Not yet implemented")
+        rv_shopping_list.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        rv_shopping_list.adapter = shoppingListAdapter
+        rv_shopping_list.setHasFixedSize(true)
+        createItemTouchHelper().attachToRecyclerView(rv_shopping_list)
     }
+
+    private fun getShoppingListFromDatabase() {
+        mainScope.launch {
+            val shoppingList = withContext(Dispatchers.IO) {
+                productRepository.getAllProducts()
+            }
+            this@ShoppingListFragment.shoppingList.clear()
+            this@ShoppingListFragment.shoppingList.addAll(shoppingList)
+            this@ShoppingListFragment.productAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        val itemTouchHelperCallback =
+            object :
+                ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                }
+
+            }
+
+        return ItemTouchHelper(itemTouchHelperCallback)
+    }
+
 }
