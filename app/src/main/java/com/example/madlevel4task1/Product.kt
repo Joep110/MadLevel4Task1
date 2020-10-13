60c99fece55124b33767dd6c67054d4639006122
Joep Boekhold <joepboekhold@gmail.com>
2020-10-13 14:26:52 +0200

    Created DAO object

--- app/src/main/java/com/example/madlevel4task1/Product.kt
+++ app/src/main/java/com/example/madlevel4task1/Product.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName =  "ProductTable")
+@Entity(tableName =  "Product_Table")
 data class Product (
 
     @ColumnInfo(name = "name")
